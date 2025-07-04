3759897c02e10893d8ff05f8207ee8c5a184d3a3
Jacob <jakevilevac@gmail.com>
2025-07-04 00:28:28 -0700

    fix: turso by default

--- src/lib/gameStore.ts
+++ src/lib/gameStore.ts
@@ -2,37 +2,37 @@ import { Game } from "@/types/game";
 import * as sqliteGameStore from "./sqliteGameStore";
 import * as tursoGameStore from "./tursoGameStore";
 
-// Toggle this constant to switch between Turso and local SQLite
-export const USE_TURSO_DB = process.env.USE_TURSO_DB === "true";
+// Use Turso by default, use local SQLite only if USE_LOCAL_DB is set to 'true'
+export const USE_LOCAL_DB = process.env.USE_LOCAL_DB === "true";
 
 export async function getGames(): Promise<Game[]> {
-	if (USE_TURSO_DB) {
-		return await tursoGameStore.getGames();
-	} else {
+	if (USE_LOCAL_DB) {
 		return sqliteGameStore.getGames();
+	} else {
+		return await tursoGameStore.getGames();
 	}
 }
 
 export async function getGame(gameCode: string): Promise<Game | undefined> {
-	if (USE_TURSO_DB) {
-		return await tursoGameStore.getGame(gameCode);
-	} else {
+	if (USE_LOCAL_DB) {
 		return sqliteGameStore.getGame(gameCode);
+	} else {
+		return await tursoGameStore.getGame(gameCode);
 	}
 }
 
 export async function setGame(gameCode: string, game: Game): Promise<void> {
-	if (USE_TURSO_DB) {
-		await tursoGameStore.saveGame(game);
-	} else {
+	if (USE_LOCAL_DB) {
 		sqliteGameStore.saveGame(game);
+	} else {
+		await tursoGameStore.saveGame(game);
 	}
 }
 
 export async function deleteGame(gameCode: string): Promise<void> {
-	if (USE_TURSO_DB) {
-		await tursoGameStore.deleteGame(gameCode);
-	} else {
+	if (USE_LOCAL_DB) {
 		sqliteGameStore.deleteGame(gameCode);
+	} else {
+		await tursoGameStore.deleteGame(gameCode);
 	}
 }
